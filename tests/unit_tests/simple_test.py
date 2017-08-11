7295f90f085fca9c002ab367ff418d42c497b303
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-08-11 22:16:37 +0530

    Add simple test

--- tests/unit_tests/simple_test.py
+++ tests/unit_tests/simple_test.py
@@ -1,12 +1,8 @@
-import pytest
-import sys 
 from aslo import init_app
 
 
 class TestSimple(object):
-    def test_simple(self):
-        print("It works")
 
     def test_if_app_starts(self):
         app = init_app()
-        assert(app)    
+        assert(app)
