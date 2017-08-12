5e6a0fcd23362cce4ca357db722492296c5eb614
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-08-12 22:09:03 +0530

    Add setup and teardown  methods to TestSimple Class

--- tests/unit_tests/simple_test.py
+++ tests/unit_tests/simple_test.py
@@ -1,8 +1,25 @@
 from aslo import init_app
-
+import os
+import subprocess
+import requests
+import time
 
 class TestSimple(object):
+    
+    @classmethod
+    def setup_class(cls):
+       # Most hackish way to run the server
+        server_pid = subprocess.Popen(['./start.sh'],shell=True)
+        print("Setup. Starting the Server")
 
+    
     def test_if_app_starts(self):
         app = init_app()
         assert(app)
+      
+
+    @classmethod
+    def teardown_class(cls):
+        os.system('killall honcho')
+        print("Teardown. Stopping the server")
+        
