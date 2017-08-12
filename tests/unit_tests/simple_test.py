a5cba7279422cede2af30189bef1e2976259edbc
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-08-12 23:22:37 +0530

    Add server interaction tests

--- tests/unit_tests/simple_test.py
+++ tests/unit_tests/simple_test.py
@@ -1,6 +1,8 @@
 from aslo import init_app
 import os
 import subprocess
+import requests
+import time
 
 
 class TestSimple(object):
@@ -9,11 +11,17 @@ class TestSimple(object):
     def setup_class(cls):
         # Most hackish way to run the server
         subprocess.Popen(['./start.sh'], shell=True)
+        # Allow grace of 5 seconds, server bootup time
+        time.sleep(5)
         print("Setup. Starting the Server")
 
     def test_if_app_starts(self):
         app = init_app()
         assert(app)
+
+    def test_app_should_respond_ok_when_index_page_is_opened(self):
+        res = requests.get("http://localhost:5000")
+        assert res.status_code == 200
 
     @classmethod
     def teardown_class(cls):
